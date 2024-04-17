265bf3868d012295b2433b636e3f733ad6da5382
annieherieau <annie.herieau@gmail.com>
2024-04-17 13:43:11 +0200

    exo 1

--- exos/exo5.js
+++ exos/exo5.js
@@ -1,3 +1,8 @@
 // SUJET 1
 // Avec une liste de nombres entiers relatifs et un nombre k, crée une méthode retournant un booléen qui affiche si deux nombres de cette liste ont k comme résultat de leur somme.
 //Par exemple, si je te donne la liste [10, 15, 3, 7] et k = 17, la fonction devra sortir true car 10 + 7 = 17. Si je te donne la liste [1, 8, 10, 21] et k = 19, la fonction devra sortir false car il n'y a pas deux nombres ayant 19 comme résultat de leur addition.
+
+const data1 = [
+  {array: [10, 15, 3, 7], k: 17, result: true},
+  {array: [1, 8, 10, 21], k: 19, result: false},
+]
