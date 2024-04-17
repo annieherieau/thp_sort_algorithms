d385c158cc4969c88240a788473c78eb1bb1a551
annieherieau <annie.herieau@gmail.com>
2024-04-17 16:46:03 +0200

    exo 2

--- exos/exo2.js
+++ exos/exo2.js
@@ -1,4 +1,4 @@
-// SUJET 2 - EXO 4
+// SUJET 2 - EXO 2
 
 // Nous allons te donner une liste contenant la hauteur (en étages) d'immeubles appartenant à une rue, d'est en ouest. Un agent immobilier voudrait que tu écrives un algorithme qui retourne combien de bâtiments de cette rue ont au moins un appartement avec une vue sur le soleil couchant (à l'ouest), afin de bien évaluer la valeur des bâtiments de la rue.
 // Par exemple, avec la liste [3, 7, 8, 3, 6, 1], la fonction devrait retourner 3, puisque l'étage le plus haut des immeubles ayant comme taille 8, 6, et 1 ont tous une vue à l'ouest. Ou autre exemple la liste [1, 4, 5, 8] devrait te retourner 1 puisque seul le dernier bâtiment a au moins un appartement avec une vue à l'ouest.
@@ -16,7 +16,7 @@ data.forEach((data) => {
   console.log("-------");
 });
 
-// complexité algorithmique de O(n) 1 boucle
+// complexité algorithmique de O(n²) : 2 boucles imbriquées
 function algo(array) {
   // le dernier bulding
   let result = [array[array.length - 1]];
@@ -29,3 +29,4 @@ function algo(array) {
   }
   return result.reverse();
 }
+
